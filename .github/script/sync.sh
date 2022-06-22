2b1b7e31d66001e7b91611a3f49e94a9abcf4746
Haku Wei <hofneondeion21@gmail.com>
2022-06-21 22:20:54 -0400

    updated script -- added known sshkey

--- .github/script/sync.sh
+++ .github/script/sync.sh
@@ -29,8 +29,8 @@
 # then
 #     echo "Branch ${branch} exists"
 # fi
-cat ~/.ssh/known_hosts;
-cat ~/.ssh/id_rsa.pub;
+touch ~/.ssh/id_rsa.pub;
+echo "bitbucket.org,2406:da00:ff00::22e9:9f55 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCz/3XxtTCqIclBevSBpf0eVzlN3HqcPt4Vy0Tub2cMtqRIgLk1Vld0tspSSvfYzjLc5Y4vDKbHwEP1RWOIW6TPmOZB2vQ7JouRo9Gk0P5MEbDbBW6FjHIadm2UAXz/oVr4blzcpaoMyeljdwze5UtsOecTWXE5jJVJ4teu6SQCwazFfejcyk735H0kjMh8EfTQ6SeGfyeTO3dhBpDBX3I7cwg5aZzDTyf5ljgoOvsb163HMwn/wqw7PnfDuc/3wOKuzGrQkn4xK90SAXG09MMoZ6Ryk3WpBOBqvAIvpMrlWUIK+Jhe/naYab59UiqXTHlv7E16kovUDDUH4P9z1IkFZ4mz1wDD5xtHmVnIX8ejzbW4CNnCE1S/B303/iYu9zBwKZZ8KAtyKe2wmMDviTw9YWbDmMh1aoC9nf1x5HT0MfpptMCtEPzgs2bJ6fP40OPUrPw5PycnNP5OUDh0CxE40v9PfMNzrW8PohNoyweiOvHbD4Kvqv9hYsVnDs1vPMc=" > ~/.ssh/id_rsa.pub;
 ssh -vT git@bitbucket.org:AnnoMMLXXVII/fantasyfranchise.git;
 git remote add sync git@bitbucket.org:AnnoMMLXXVII/fantasyfranchise.git;
 git remote -v;
