5e93f4ae7ce0151ef818a2db735a3aef9a0fd78d
Haku Wei <hofneondeion21@gmail.com>
2022-06-21 22:14:40 -0400

    updated script ssh url -- atempt 2

--- .github/script/sync.sh
+++ .github/script/sync.sh
@@ -29,7 +29,7 @@
 # then
 #     echo "Branch ${branch} exists"
 # fi
-ssh -vT bitbucket.org:AnnoMMLXXVII/fantasyfranchise.git;
-git remote add sync bitbucket.org:AnnoMMLXXVII/fantasyfranchise.git;
+ssh -vT git@bitbucket.org:AnnoMMLXXVII/fantasyfranchise.git;
+git remote add sync git@bitbucket.org:AnnoMMLXXVII/fantasyfranchise.git;
 git remote -v;
 git push sync;
