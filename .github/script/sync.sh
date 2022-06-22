87e474eebedce2db81a3f3ee24ae3ae3bfb47b0e
Haku Wei <hofneondeion21@gmail.com>
2022-06-21 21:49:23 -0400

    updated sync.sh script

--- .github/script/sync.sh
+++ .github/script/sync.sh
@@ -1,35 +1,35 @@
 #! /bin/bash
 
-if ! currentbranch=$(git symbolic-ref --short -q HEAD)
-then
-	echo We are not currently on a branch.
-	exit 1
-fi
+# if ! currentbranch=$(git symbolic-ref --short -q HEAD)
+# then
+# 	echo We are not currently on a branch.
+# 	exit 1
+# fi
 
-# Uncommited Changes - Exit script if there uncommited changes
-if ! git diff-index --quiet HEAD --; then
-	echo "There are uncommited changes on this repository."
-	exit 1
-fi
+# # Uncommited Changes - Exit script if there uncommited changes
+# if ! git diff-index --quiet HEAD --; then
+# 	echo "There are uncommited changes on this repository."
+# 	exit 1
+# fi
 
-# Remote exists - Exit script if remote does not exist
-git ls-remote --exit-code ${remote} >/dev/null 2>&1
-if [ $? -ne 0 ]
-then
-	echo "Remote ${remote} does not exist"
-    exit 1
-fi
+# # Remote exists - Exit script if remote does not exist
+# git ls-remote --exit-code ${remote} >/dev/null 2>&1
+# if [ $? -ne 0 ]
+# then
+# 	echo "Remote ${remote} does not exist"
+#     exit 1
+# fi
 
-# Root path of Repo
-rootpath=$(git rev-parse --show-toplevel)
+# # Root path of Repo
+# rootpath=$(git rev-parse --show-toplevel)
 
-# Branch exists - Exit script if local branch does not exist
-git rev-parse -q --verify ${branch} >/dev/null 2>&1
-if [ $? -ne 0 ]
-then
-    echo "Branch ${branch} exists"
-fi
+# # Branch exists - Exit script if local branch does not exist
+# git rev-parse -q --verify ${branch} >/dev/null 2>&1
+# if [ $? -ne 0 ]
+# then
+#     echo "Branch ${branch} exists"
+# fi
 
-# git remote add sync https://AnnoMMLXXVII@bitbucket.org/AnnoMMLXXVII/fantasyfranchise.git;
-# git remote -v;
-# git push sync;
+git remote add sync https://AnnoMMLXXVII@bitbucket.org/AnnoMMLXXVII/fantasyfranchise.git;
+echo `git remote -v`;
+git push sync;
