65f0c9ce45f9a77dc1775d5404eda3ed0ca4fd6e
jruscito <jruscito@uri.edu>
2022-07-07 22:26:29 -0400

    Test 1

--- FantasyFranchiseAPI/src/test/java/com/fantasy/franchise/player/PlayerTest.java
+++ FantasyFranchiseAPI/src/test/java/com/fantasy/franchise/player/PlayerTest.java
@@ -77,5 +77,7 @@ class PlayerTest {
     void checkNotTooOld(){
         int age = 2022 - test.getBirthDate().getYear();
         assertThat(age).as("Too old").isLessThanOrEqualTo(50);
+        System.out.println("Test");
     }
+
 }
