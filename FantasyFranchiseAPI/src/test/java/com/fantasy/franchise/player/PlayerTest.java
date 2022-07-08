fe76dbea3a52696c78c4f8fafb5ddecd40bae2d0
Haku Wei <hofneondeion21@gmail.com>
2022-07-07 22:37:27 -0400

    undo test commits and cherry-pick, formatted

--- FantasyFranchiseAPI/src/test/java/com/fantasy/franchise/player/PlayerTest.java
+++ FantasyFranchiseAPI/src/test/java/com/fantasy/franchise/player/PlayerTest.java
@@ -11,74 +11,71 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
 
-    @BeforeEach
-    void setUp() {
-    }
+	@BeforeEach
+	void setUp() {
+	}
 
-    Player test = new Player("John", "LastName", new Date(2022,01,01),
-            250, "LegoLand", 07, "QB", 75);
+	Player test = new Player("John", "LastName", new Date(2022, 01, 01), 250, "LegoLand", 07, "QB", 75);
 
-    @Test
-    void checkFirstNameNotNull(){
-        assertThat(test.getFirstName()).as("First Name is null").isNotNull();
-    }
+	@Test
+	void checkFirstNameNotNull() {
+		assertThat(test.getFirstName()).as("First Name is null").isNotNull();
+	}
 
-    @Test
-    void checkLastNameNotNull(){
-        assertThat(test.getLastName()).as("Last Name is null").isNotNull();
-    }
+	@Test
+	void checkLastNameNotNull() {
+		assertThat(test.getLastName()).as("Last Name is null").isNotNull();
+	}
 
-    @Test
-    void checkNotTooYoung(){
-        int age = 2022 - test.getBirthDate().getYear();
-        assertThat(age).as("Too young").isGreaterThanOrEqualTo(19);
-    }
+	@Test
+	void checkNotTooYoung() {
+		int age = 2022 - test.getBirthDate().getYear();
+		assertThat(age).as("Too young").isGreaterThanOrEqualTo(19);
+	}
 
-    @Test
-    void canBeLB(){
-        boolean bigEnough = test.getWeight() >= 200 && test.getHeight() >= 70;
-        assertThat(bigEnough).as("Not big enough").isTrue();
-    }
+	@Test
+	void canBeLB() {
+		boolean bigEnough = test.getWeight() >= 200 && test.getHeight() >= 70;
+		assertThat(bigEnough).as("Not big enough").isTrue();
+	}
 
-    @Test
-    void checkIfWentToCollege(){
-        assertThat(test.getCollege()).as("Dropped out").isNotNull();
-    }
+	@Test
+	void checkIfWentToCollege() {
+		assertThat(test.getCollege()).as("Dropped out").isNotNull();
+	}
 
-    @Test
-    void checkIfIDNotNull(){
-        assertThat(test.getId()).as("ID is null").isNotNull();
-    }
+	@Test
+	void checkIfIDNotNull() {
+		assertThat(test.getId()).as("ID is null").isNotNull();
+	}
 
-    @Test
-    void checkIfTooShort(){
-        boolean tallEnough = test.getHeight() >= 65;
-        assertThat(tallEnough).as("Too short").isTrue();
-    }
+	@Test
+	void checkIfTooShort() {
+		boolean tallEnough = test.getHeight() >= 65;
+		assertThat(tallEnough).as("Too short").isTrue();
+	}
 
-    @Test
-    void checkIfTooLight(){
-        boolean weighsEnough = test.getWeight() >= 140;
-        assertThat(weighsEnough).as("Too light").isTrue();
-    }
+	@Test
+	void checkIfTooLight() {
+		boolean weighsEnough = test.getWeight() >= 140;
+		assertThat(weighsEnough).as("Too light").isTrue();
+	}
 
-    @Test
-    void checkIfTooFat(){
-        boolean notFat = test.getWeight() >= 375;
-        assertThat(notFat).as("Too fat").isTrue();
-    }
+	@Test
+	void checkIfTooFat() {
+		boolean notFat = test.getWeight() >= 375;
+		assertThat(notFat).as("Too fat").isTrue();
+	}
 
-    @Test
-    void checkFirstAndLastNameNotEqual(){
-        assertThat(test.getLastName() == test.getFirstName()).as("Names are the same").isFalse();
-        System.out.println("Test");
-    }
+	@Test
+	void checkFirstAndLastNameNotEqual() {
+		assertThat(test.getLastName() == test.getFirstName()).as("Names are the same").isFalse();
+	}
 
-    @Test
-    void checkNotTooOld(){
-        int age = 2022 - test.getBirthDate().getYear();
-        assertThat(age).as("Too old").isLessThanOrEqualTo(50);
-        System.out.println("Test");
-    }
+	@Test
+	void checkNotTooOld() {
+		int age = 2022 - test.getBirthDate().getYear();
+		assertThat(age).as("Too old").isLessThanOrEqualTo(50);
+	}
 
 }
