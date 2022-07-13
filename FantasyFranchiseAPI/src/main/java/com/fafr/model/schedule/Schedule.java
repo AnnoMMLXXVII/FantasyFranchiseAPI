a1873af9117ec50aed0eb57391334ad41a4b614e
jruscito <jruscito@uri.edu>
2022-07-12 23:36:14 -0400

    changed how home & away are counted

--- FantasyFranchiseAPI/src/main/java/com/fafr/model/schedule/Schedule.java
+++ FantasyFranchiseAPI/src/main/java/com/fafr/model/schedule/Schedule.java
@@ -85,60 +85,98 @@ public class Schedule {
 
 
     // Methods
-
-    public boolean ruleOneFulfilled(Team thisTeam, List<Team> scheduledTeams, int gamesAtHome, int gamesAway) {
+    public boolean ruleOneFulfilled(Team thisTeam, List<Team> scheduledTeams,List<Character> location) {
+        int homeCounter = 0;
+        int awayCounter = 0;
         int divCounter = 0;
-        boolean sixGamesInDiv = false;
+        boolean sixGamesinDiv = false;
         String teamsDiv = thisTeam.getDivision();
 
-        for (Team currTeam : scheduledTeams) {
-            if (currTeam.getDivision().equals(teamsDiv)) {
+        for (int i = 0; i < scheduledTeams.size() ; i++) {
+
+            // Check if not in same division
+            Team currTeam = scheduledTeams.get(i);
+            if (currTeam.getDivision().equals(teamsDiv)){
                 divCounter += 1;
                 if (divCounter == 6) {
-                    sixGamesInDiv = true;
+                    sixGamesinDiv = true;
                     break;
                 }
             }
-        }
 
-        return (sixGamesInDiv && gamesAtHome >= 1 && gamesAway >= 1);
+            // Check if their home or away
+            if(location.get(i) == 'H'){
+                homeCounter += 1;
+            }else if(location.get(i) == 'A'){
+                awayCounter += 1;
+            }
+        } // end for
+
+        return (sixGamesinDiv && homeCounter >= 2 && awayCounter >= 2);
     }
-
-    public boolean ruleTwoFulfilled(Team thisTeam, List<Team> scheduledTeams, int gamesAtHome, int gamesAway) {
+                                                                                // Change to object
+    public boolean ruleTwoFulfilled(Team thisTeam, List<Team> scheduledTeams, List<Character> location) {
+        int homeCounter = 0;
+        int awayCounter = 0;
         int conferenceCounter = 0;
         boolean fourGamesInConf = false;
         String teamsConf = thisTeam.getConference();
 
-        for (Team currTeam : scheduledTeams) {
-            if (currTeam.getConference().equals(teamsConf)) {
+        for (int i = 0; i < scheduledTeams.size() ; i++) {
+
+            // Check if not in same confrence
+            Team currTeam = scheduledTeams.get(i);
+            if (currTeam.getConference().equals(teamsConf)){
                 conferenceCounter += 1;
                 if (conferenceCounter == 4) {
                     fourGamesInConf = true;
                     break;
                 }
             }
-        }
 
-        return (fourGamesInConf && gamesAtHome >= 2 && gamesAway >= 2);  // change away and home counter to find
-                                                                        // a way to track which team is home or away
+            // Check if their home or away
+            if(location.get(i) == 'H'){
+                homeCounter += 1;
+            }else if(location.get(i) == 'A'){
+                awayCounter += 1;
+            }
+        } // end for
+
+        return (fourGamesInConf && homeCounter >= 2 && awayCounter >= 2);
     }
 
-    public boolean ruleThreeFulfilled(Team thisTeam, List<Team> scheduledTeams, int gamesAtHome, int gamesAway) {
+
+    // RAVENSSCHED = [BUCKS, PATS, STEELERS]\
+    // RAVENSLOC  =  [H ,    A,     H]
+                                                                                // Change to object
+    public boolean ruleThreeFulfilled(Team thisTeam, List<Team> scheduledTeams, List<Character> location){
+        int homeCounter = 0;
+        int awayCounter = 0;
         int conferenceCounter = 0;
         boolean fourGamesInConf = false;
         String teamsConf = thisTeam.getConference();
 
-        for (Team currTeam : scheduledTeams) {
-            if (!(currTeam.getConference().equals(teamsConf))) {
+        for (int i = 0; i < scheduledTeams.size() ; i++) {
+
+            // Check if not in same confrence
+            Team currTeam = scheduledTeams.get(i);
+            if (!currTeam.getConference().equals(teamsConf)){
                 conferenceCounter += 1;
                 if (conferenceCounter == 4) {
                     fourGamesInConf = true;
                     break;
                 }
             }
-        }
 
-        return (fourGamesInConf && gamesAtHome >= 2 && gamesAway >= 2);
+            // Check if their home or away
+            if(location.get(i) == 'H'){
+                homeCounter += 1;
+            }else if(location.get(i) == 'A'){
+                awayCounter += 1;
+            }
+        } // end for
+
+        return (fourGamesInConf && homeCounter >= 2 && awayCounter >= 2);
     }
 
 
